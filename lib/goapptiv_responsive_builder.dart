c17f5da6870c915741a47c7c31521882cdd93e0f
BiswajitKrP <88763246+BiswajitKrP@users.noreply.github.com>
2022-02-25 12:23:09 +0530

    responsive_builder name changed to screen_type_builder

--- lib/goapptiv_responsive_builder.dart
+++ lib/goapptiv_responsive_builder.dart
@@ -2,4 +2,4 @@ library goapptiv_responsive_builder;
 
 export 'package:goapptiv_responsive_builder/src/mediaquery_extensions.dart';
 export 'package:goapptiv_responsive_builder/src/orientation_layout_builder.dart';
-export 'package:goapptiv_responsive_builder/src/responsive_builder.dart';
+export 'package:goapptiv_responsive_builder/src/screen_type_builder.dart';
